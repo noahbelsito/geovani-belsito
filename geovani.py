9ce5ccbebad914790c623342dfadf4c717bdfbea
Noah Belsito <nbelsito02@gmail.com>
2021-06-28 03:57:35 -0400

    update

--- geovani.py
+++ geovani.py
@@ -5,20 +5,29 @@ from chatterbot.trainers import ListTrainer
 geovani_bot = ChatBot(
     "Geovani Belsito",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",  # storage adapter
-    logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter'
-    ],
+    # logic_adapters=[
+    #     'chatterbot.logic.MathematicalEvaluation',
+    #     'chatterbot.logic.TimeLogicAdapter'
+    # ],
     database_uri="sqlite:///geovani-brain.sqlite3"  # name of storage
 )
 
-#  trainer = ListTrainer(geovani_bot)
-#  trainer.train([
-#  "",
-#  "",
-#  ""
-#  ])
+# reinforced training
+"""
+trainer = ListTrainer(geovani_bot)
 
+trainer.train([
+    ""
+])
+
+count = 0
+while count < 200:
+    response = geovani_bot.get_response("Good")
+    count += 1
+    print(count)
+"""
+
+# talking raw to my bot
 while True:
     try:
         bot_input = geovani_bot.get_response(input())
